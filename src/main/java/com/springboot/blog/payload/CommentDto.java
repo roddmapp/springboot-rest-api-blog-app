c071daf4c38add6a877735cf06cd167ba7edb381
Rodney <rodney.mapako@synthesis.com>
2022-10-02 21:02:07 +0200

    Added api validation for CommentDto and CommentController

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -2,10 +2,27 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
     private long id;
+
+    //name  should not be null of empty
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
+
+    //email should not be null or empty
+    //email field validation
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
     private String email;
+
+    //comment body must not be null or empty
+    //comment body must be minimum 10 characters
+    @NotEmpty
+    @Size(min = 10, message = "Comment body must be minimum 10 characters")
     private String body;
 }
